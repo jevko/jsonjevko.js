2d4612155c8ef50e8120f71d05676d47d457b9da
Dariusz Jędrzejczak <dariusz.jedrzejczak.work+github@gmail.com>
2022-02-07 22:42:34 +0100

    initial

---
+++ test.js
@@ -0,0 +1,31 @@
+import {jsonToJevko} from './mod.js'
+import {jevkoToPrettyString} from 'jevkoutils.js'
+
+console.assert(jevkoToPrettyString(jsonToJevko({ 
+  "first name": "John",
+  "last name": "Smith",
+  "is alive": true,
+  "age": 27,
+  "address": {
+    "street address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal code": "10021-3100"
+  },
+  "phone numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [],
+  "spouse": null 
+})).includes(`
+  [
+    type [home]
+    number [212 555-1234]
+  ]`))
