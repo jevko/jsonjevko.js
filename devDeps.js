2bb8932de719cf1dddaf09f8f50116a7f1b9084d
Dariusz Jędrzejczak <dariusz.jedrzejczak.work+github@gmail.com>
2022-03-04 05:55:06 +0100

    deno test

--- devDeps.js
+++ devDeps.js
@@ -1,1 +1,2 @@
 export {jevkoToPrettyString} from 'https://cdn.jsdelivr.net/gh/jevko/jevkoutils.js@v0.1.4/mod.js'
+export {assert} from 'https://deno.land/std@0.128.0/testing/asserts.ts'
--- test.js
+++ test.js
@@ -1,58 +1,65 @@
 import {jsonToJevko, jsonToSchema} from './mod.js'
-import {jevkoToPrettyString} from './devDeps.js'
+import {jevkoToPrettyString, assert} from './devDeps.js'
 
-console.assert(jevkoToPrettyString(jsonToJevko({ 
-  "first name": "John",
-  "last name": "Smith",
-  "is alive": true,
-  "age": 27,
-  "address": {
-    "street address": "21 2nd Street",
-    "city": "New York",
-    "state": "NY",
-    "postal code": "10021-3100"
-  },
-  "phone numbers": [
-    {
-      "type": "home",
-      "number": "212 555-1234"
+const {test} = Deno
+
+test('jsonToJevko', () => {
+  assert(jevkoToPrettyString(jsonToJevko({ 
+    "first name": "John",
+    "last name": "Smith",
+    "is alive": true,
+    "age": 27,
+    "address": {
+      "street address": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postal code": "10021-3100"
     },
-    {
-      "type": "office",
-      "number": "646 555-4567"
-    }
-  ],
-  "children": [],
-  "spouse": null 
-})).includes(`
+    "phone numbers": [
+      {
+        "type": "home",
+        "number": "212 555-1234"
+      },
+      {
+        "type": "office",
+        "number": "646 555-4567"
+      }
+    ],
+    "children": [],
+    "spouse": null 
+  })).includes(`
   [
     type [home]
     number [212 555-1234]
   ]`))
-
-const res = jsonToSchema({ 
-  "first name": "John",
-  "last name": "Smith",
-  "is alive": true,
-  "age": 27,
-  "address": {
-    "street address": "21 2nd Street",
-    "city": "New York",
-    "state": "NY",
-    "postal code": "10021-3100"
-  },
-  "phone numbers": [
-    {
-      "type": "home",
-      "number": "212 555-1234"
-    },
-    {
-      "type": "office",
-      "number": "646 555-4567"
-    }
-  ],
-  "children": [],
-  "spouse": null 
 })
 
-console.assert(res.props['phone numbers'].itemSchemas[0].props.number.type === 'string')
+test('jsonToSchema', () => {
+  const res = jsonToSchema({ 
+    "first name": "John",
+    "last name": "Smith",
+    "is alive": true,
+    "age": 27,
+    "address": {
+      "street address": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postal code": "10021-3100"
+    },
+    "phone numbers": [
+      {
+        "type": "home",
+        "number": "212 555-1234"
+      },
+      {
+        "type": "office",
+        "number": "646 555-4567"
+      }
+    ],
+    "children": [],
+    "spouse": null 
+  })
+  
+  assert(res.props['phone numbers'].itemSchemas[0].props.number.type === 'string')
+})
+
